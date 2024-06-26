92492a240aba0bf256aef3f15464663d7a5d7df0
Omegah Junior <61554178+omega-cpu@users.noreply.github.com>
2024-06-26 12:33:15 +0100

    Add files via upload
    Group project1

---
+++ src/main/java/com/mycompany/porjectwork1/Porjectwork1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.porjectwork1;
+
+/**
+ *
+ * @author oem
+ */
+public class Porjectwork1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
